8e1fd540dc81a50ea9e199065c9fa705f44337ba
Dbridg <D.bridges721@gmail.com>
2019-08-28 11:02:13 -0400

    first commit

---
+++ Part 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm from Sacramento, California.");
+        }
+    }
+}
---
+++ Part 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("          ");
+            Console.WriteLine("Hello MAD students - I'm Dylan Bridges");
+            Console.WriteLine("          ");
+            Console.WriteLine("I'm from Sacramento, California.");
+            Console.WriteLine("          ");
+        }
+    }
+}
